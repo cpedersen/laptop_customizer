f2694d42f663c7cc4b83f3959e8a258020e79cb5
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-04-13 22:28:39 -0400

    Convert Feature_Item from class to function

--- src/Feature_Item.js
+++ src/Feature_Item.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 
-class Feature_Item extends Component {
-
-    render() {
-        console.log("Made it inside Feature_Item")
-        return (
-            <legend className="feature__name">
-                <h3>{this.props.feature}</h3>
-          </legend>
-
-
-
-        )
-    }
-
+function Feature_Item(props) {
+    console.log("Made it inside Feature_Item")
+    return (
+        <legend className="feature__name">
+            <h3>{props.feature}</h3>
+        </legend>
+    )
 }
 
 export default Feature_Item;
